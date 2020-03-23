f804a191eff1a013473a21ffd26e34736b79b0ff
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-03-23 12:15:40 -0500

    character movement added

--- Cats-vs-Dogs/src/app/app.component.ts
+++ Cats-vs-Dogs/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cats-vs-Dogs';
-  page = "login";
+  page = "init";
 
 
   changePage(page:string)
--- Cats-vs-Dogs/src/app/battle-screen/battle-screen.component.ts
+++ Cats-vs-Dogs/src/app/battle-screen/battle-screen.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+var num = 0;
+
+document.onkeydown = function(e)
+{
+ // alert('test')
+  if(e.keyCode == 39){
+    num+=10;
+  }
+  else if(e.keyCode == 37){
+    num-=10;
+  }
+  
+  document.getElementById("player").style.left =  (num +"px");
+}
+
 @Component({
   selector: 'app-battle-screen',
   templateUrl: './battle-screen.component.html',
