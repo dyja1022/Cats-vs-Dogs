45c2c3edc74097fa0907b5b1689432c6e074a70e
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-03-23 02:45:43 -0500

    put controls in frame to toggle pages

--- Cats-vs-Dogs/src/app/app.component.ts
+++ Cats-vs-Dogs/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Cats-vs-Dogs';
   page = "login";
+
+
+  changePage(page:string)
+  {
+    this.page = page;
+  }
 }
