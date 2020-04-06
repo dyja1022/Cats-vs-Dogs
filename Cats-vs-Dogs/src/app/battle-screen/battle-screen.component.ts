cda948a7f6dc919dbf3edffaf97bffbd38aa9d2f
Jameson Dy <dyjameson22@gmail.com>
2020-04-05 17:52:41 -0700

    a

--- Cats-vs-Dogs/src/app/battle-screen/battle-screen.component.ts
+++ Cats-vs-Dogs/src/app/battle-screen/battle-screen.component.ts
@@ -235,7 +235,7 @@ export class BattleScreenComponent implements OnInit {
     let background = document.getElementById("battle-map");
 
     setTimeout(() => { 
-       background.style.filter = 'invert(25%) sepia(94%) saturate(5316%) hue-rotate(357deg) brightness(107%) contrast(96%)'
+       background.style.filter = 'invert(25%) sepia(94%) saturate(8316%) hue-rotate(357deg) brightness(807%) contrast(96%)'
        background.style.transition = '4s'
       }, 3500); 
 
